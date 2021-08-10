b39f5cbcf3b14811e7802fdb9452d374ea0f10e4
Jagat Kafle <43719985+jakafle@users.noreply.github.com>
2021-08-10 16:20:05 +0545

    Update fibonacci.sh

--- fibonacci.sh
+++ fibonacci.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-# File: fib.sh
+# File: fibonacci.sh
 
-function fib {
+# To run this function follow the following steps in the terminal inside the directory which contains this file.
+# 1. source fibonacci.sh
+# 2. fibo N
+# Here, N is the number you pass as an argument to the command. The command prints first N fibonacci numbers.
+
+function fibo {
 	a=0
 	b=1
 	for i in {1..$(eval echo {1..$1})}
